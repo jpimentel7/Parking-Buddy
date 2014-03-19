e448dbdbee908c3eac06355e14931b71517696b9
jpimentel7 <javier.pimentel.791@gmail.com>
2014-03-18 20:43:26 -0700

    I imported the google library and made a map display

--- src/com/example/ParkingBuddy/MyActivity.java
+++ src/com/example/ParkingBuddy/MyActivity.java
@@ -5,6 +5,8 @@ import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -30,24 +32,59 @@ public class MyActivity extends Activity {
         String provider= locationManager.getBestProvider(criteria,true);
         Location location= locationManager.getLastKnownLocation(provider);
         */
+        //get the last gps based locatoin
         LocationManager locationManager=(LocationManager)getSystemService(getApplicationContext().LOCATION_SERVICE);
         Location location =locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
         Toast toast=Toast.makeText(getApplicationContext(),location.toString(),Toast.LENGTH_LONG);
         toast.show();
         //starts google maps
         GoogleMap map=((MapFragment)getFragmentManager().findFragmentById(R.id.map)).getMap();
-        map.setMyLocationEnabled(true);
-        //centers it to our last know location
+               //centers it to our last know location
         CameraPosition cameraPosition = new CameraPosition.Builder().target(
                 new LatLng(location.getLatitude(),location.getLongitude())).zoom(17).build();
         map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
-
-
-
-
-
+        //
+        Button saveLocation=(Button)findViewById(R.id.save);
+        Button clearLocation=(Button)findViewById(R.id.clear);
+        saveLocation.setOnClickListener(handler1);
+        clearLocation.setOnClickListener(handler2);
     }
 
+    View.OnClickListener handler1 = new View.OnClickListener(){
+        @Override
+        public void onClick(View view) {
 
+        }
+    };
+    View.OnClickListener handler2 = new View.OnClickListener(){
+        @Override
+        public void onClick(View view){
 
+        }
+    };
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
 }
